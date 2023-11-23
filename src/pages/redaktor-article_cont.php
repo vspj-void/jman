af07f64a0c0461f75a553a7e3324ede18b5036eb
vencel64 <flashex64@gmail.com>
2023-11-23 08:27:23 +0100

    Fix select author as whole team

--- src/pages/redaktor-article_cont.php
+++ src/pages/redaktor-article_cont.php
@@ -1,4 +1,3 @@
-
 <?php
 require_once "includes/db_connect.php";
 require_once "classes/profil.php";
@@ -49,14 +48,14 @@ if (isset($_GET['action']) && $_GET['action'] == 'rejectArticle' && isset($_GET[
     $articleName = $rowArticleName['NAZEV'];
     $queryAutorIDs = "SELECT A.ID_OSOBY FROM AUTORI A JOIN OSOBA O ON A.ID_OSOBY=O.ID WHERE ID_PRISPEVKU=" . $articleId . " AND O.LOGIN IS NOT NULL";
     $resultAutorIDs = $mysqli->query($queryAutorIDs);
-    
+
     $od = $loggedUser->getId();
     $predmet = "Zamítnutí článku: " . $articleName;
     $text = "Váš článek " . $articleName . " byl zamítnut.";
 
     if ($resultAutorIDs) {
         while ($row = $resultAutorIDs->fetch_assoc()) {
-            $autorId = $row['ID_OSOBY'];         
+            $autorId = $row['ID_OSOBY'];
             $queryInsertZprava = "INSERT INTO ZPRAVY (OD, KOMU, PREDMET, TEXT) VALUES (?, ?, ?, ?)";
             $stmt = $mysqli->prepare($queryInsertZprava);
             $stmt->bind_param("iiss",  $od, $autorId, $predmet, $text);
@@ -90,15 +89,15 @@ if (isset($_POST["articleSubmit"])) {
 <div class="container mt-4">
     <div class="jumbotron">
         <h1 class="display-4">Redaktor</h1>
-        <p class="lead">nově přidané články</p>
+        <p class="lead">Nově přidané články</p>
         <hr class="my-4">
     </div>
 </div>
-<br/>
+<br />
 <div class="container mt-6 d-flex align-items-center">
     <form method="GET" action="?page=<?= $page; ?>" class="row">
         <label for="searchInput" class="col-md-4">Vyhledejte si článek:</label>
-        <div class="form-group col-md-9">  <!--šířka vyhledávacího pole-->
+        <div class="form-group col-md-9"> <!--šířka vyhledávacího pole-->
             <input type="text" class="form-control" id="searchInput" name="search" placeholder="Zadejte název článku, nebo příjmení autora">
         </div>
         <div class="col-md-1">
@@ -109,24 +108,39 @@ if (isset($_POST["articleSubmit"])) {
 
 <?php
 // Přidaný kód pro řazení a vyhledávání
-$searchTerm = isset($_GET['search']) ? $_GET['search'] : ''; 
+$searchTerm = isset($_GET['search']) ? strval($_GET['search']) : null;
 $sortBy = isset($_GET['sort']) ? $_GET['sort'] : 'NAZEV'; // defaultní řazení podle Názvu
 $sortOrder = isset($_GET['order']) && strtoupper($_GET['order']) === 'DESC' ? 'DESC' : 'ASC';
 
 // Dotaz pro získání článků s možností řazení
-$queryArticles = "SELECT PV.*, O.JMENO as AUTOR_JMENO, O.PRIJMENI as AUTOR_PRIJMENI, C.TEMA as CASOPIS_TEMA 
-                  FROM PRISPEVEKVER PV
-                  INNER JOIN PRISPEVEK P ON PV.ID_PRISPEVKU = P.ID
-                  INNER JOIN AUTORI A ON P.ID = A.ID_PRISPEVKU
-                  INNER JOIN OSOBA O ON A.ID_OSOBY = O.ID
-                  INNER JOIN CASOPIS C ON P.ID_CASOPISU = C.ID
-                  WHERE P.STAV = 1";
+$queryArticlesBase = "
+SELECT
+    DISTINCT PV.NAZEV,
+    PV.ID_PRISPEVKU,
+    GROUP_CONCAT(CONCAT(O.JMENO, ' ', O.PRIJMENI) SEPARATOR ', ') AS AUTORSKY_TYM,
+    C.TEMA AS CASOPIS_TEMA
+FROM
+    PRISPEVEKVER PV
+    INNER JOIN PRISPEVEK P ON PV.ID_PRISPEVKU = P.ID
+    INNER JOIN AUTORI A ON P.ID = A.ID_PRISPEVKU
+    INNER JOIN OSOBA O ON A.ID_OSOBY = O.ID
+    INNER JOIN CASOPIS C ON P.ID_CASOPISU = C.ID
+WHERE
+    P.STAV = 1
+GROUP BY
+    P.ID,
+    PV.VERZE";
 
 // Pokud je zadán vyhledávací termín
-if (!empty($searchTerm)) {
-    $queryArticles .= " AND (PV.NAZEV LIKE '%$searchTerm%' OR O.PRIJMENI LIKE '%$searchTerm%')";
+if (!is_null($searchTerm)) {
+    $searchCondition = "ZPV.NAZEV LIKE '%$searchTerm%' OR AUTORSKY_TYM LIKE '%$searchTerm%'";
+
+    $queryArticlesBase = "
+            SELECT * FROM ($queryArticlesBase) AS ZPV
+            WHERE $searchCondition";
 }
 
+$queryArticles = $queryArticlesBase;
 $queryArticles .= " ORDER BY $sortBy $sortOrder LIMIT $offset, $articlesPerPage";
 
 $resultArticles = $mysqli->query($queryArticles);
@@ -134,12 +148,12 @@ $resultArticles = $mysqli->query($queryArticles);
 ?>
 
 <script type="text/javascript">
-// potvrzovací okno na zveřejnění / zamítnutí článku
-function confirmAction(url, message) {
-    if (confirm(message)) {
-        window.location.href = url;
+    // potvrzovací okno na zveřejnění / zamítnutí článku
+    function confirmAction(url, message) {
+        if (confirm(message)) {
+            window.location.href = url;
+        }
     }
-}
 </script>
 
 <div class="container mt-4">
@@ -148,7 +162,7 @@ function confirmAction(url, message) {
             <tr>
                 <!-- Odkazy na řazení -->
                 <th scope="col"><a href="?page=<?= $page; ?>&sort=NAZEV&order=<?= ($sortBy === 'NAZEV' && $sortOrder === 'ASC') ? 'DESC' : 'ASC'; ?>">Název</a></th>
-                <th scope="col"><a href="?page=<?= $page; ?>&sort=AUTOR_PRIJMENI&order=<?= ($sortBy === 'AUTOR_PRIJMENI' && $sortOrder === 'ASC') ? 'DESC' : 'ASC'; ?>">Autor</a></th>
+                <th scope="col"><a href="?page=<?= $page; ?>&sort=AUTORSKY_TYM&order=<?= ($sortBy === 'AUTORSKY_TYM' && $sortOrder === 'ASC') ? 'DESC' : 'ASC'; ?>">Autor</a></th>
                 <th scope="col"><a href="?page=<?= $page; ?>&sort=TEMA&order=<?= ($sortBy === 'TEMA' && $sortOrder === 'ASC') ? 'DESC' : 'ASC'; ?>">Téma časopisu</a></th>
                 <th scope="col">Otevřít článek</th>
                 <th scope="col">Akce</th> <!-- Nový sloupec pro tlačítko -->
@@ -156,10 +170,10 @@ function confirmAction(url, message) {
         </thead>
         <tbody>
             <?php $sortBy = isset($_GET['sort']) ? $_GET['sort'] : 'NAZEV'; // defaultní řazení podle Názvu
- while ($rowArticle = $resultArticles->fetch_assoc()) : ?>
+            while ($rowArticle = $resultArticles->fetch_assoc()) : ?>
                 <tr>
                     <td><?= !empty($rowArticle["NAZEV"]) ? $rowArticle["NAZEV"] : "Název není k dispozici"; ?></td>
-                    <td><?= isset($rowArticle["AUTOR_JMENO"]) && isset($rowArticle["AUTOR_PRIJMENI"]) ? $rowArticle["AUTOR_JMENO"] . " " . $rowArticle["AUTOR_PRIJMENI"] : "Autor není k dispozici"; ?></td>
+                    <td><?= isset($rowArticle["AUTORSKY_TYM"]) && isset($rowArticle["AUTORSKY_TYM"]) ? $rowArticle["AUTORSKY_TYM"] : "Autor není k dispozici"; ?></td>
                     <td><?= !empty($rowArticle["CASOPIS_TEMA"]) ? $rowArticle["CASOPIS_TEMA"] : "Téma není k dispozici"; ?></td>
                     <td><a href="<?= isset($rowArticle["CESTA"]) ? (UPLOAD_ARTICLES_URL . "/") . $rowArticle["CESTA"] : "#"; ?>" class="btn btn-primary" target="_blank">Otevřít článek</a></td>
                     <td>
@@ -202,12 +216,10 @@ function confirmAction(url, message) {
 <div class="container">
     <ul class="pagination justify-content-center">
         <?php
-        $totalArticlesQuery = "SELECT COUNT(*) as total FROM PRISPEVEKVER PV
-                               INNER JOIN PRISPEVEK P ON PV.ID_PRISPEVKU = P.ID
-                               INNER JOIN AUTORI A ON P.ID = A.ID_PRISPEVKU
-                               WHERE P.STAV = 1";
+        $totalArticlesQuery = "SELECT COUNT(*) total FROM ($queryArticlesBase) articles";
         $totalArticlesResult = $mysqli->query($totalArticlesQuery);
         $totalArticles = $totalArticlesResult->fetch_assoc()['total'];
+
         $totalPages = ceil($totalArticles / $articlesPerPage);
 
         for ($i = 1; $i <= $totalPages; $i++) :
