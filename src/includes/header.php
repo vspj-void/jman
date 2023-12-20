fe2e0db8333d214873420663c7bbed134ed9349c
Karel Libal <122623868+karelji@users.noreply.github.com>
2023-12-20 20:24:58 +0100

    Add files via upload
    Přidán helpdesk - modální okno

--- src/includes/header.php
+++ src/includes/header.php
@@ -10,8 +10,10 @@ require_once "endpoints/get_unread_messages_count.php";
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-            <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav me-auto">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#helpdeskModal">Helpdesk</a>
+                    </li>
             <?php if (isset($_SESSION[SESSION_VAR_USER_ROLE])) : ?>
                 <?php if ($_SESSION[SESSION_VAR_USER_ROLE] == 'Autor') : ?>
                     <li class="nav-item">
@@ -28,9 +30,9 @@ require_once "endpoints/get_unread_messages_count.php";
                     <li class="nav-item">
                         <a class="nav-link" href="recenzent-article.php">Články k recenzi</a>
                     </li>
-                    <li>
+                    <!--<li>
                         <a class="nav-link" href="recenzent-statement-correction.php">Komunikace s autory</a>
-                    </li>
+                    </li> -->
                     <li>
                         <a class="nav-link" href="recenzent-namitka.php">Námitky</a>
                     </li>
@@ -110,6 +112,76 @@ require_once "endpoints/get_unread_messages_count.php";
         </div>
     </div>
 </div>
+
+<!-- Modální okno pro Helpdesk -->
+<div class="modal fade" id="helpdeskModal" tabindex="-1" aria-labelledby="helpdeskModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="helpdeskModalLabel">Helpdesk</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="helpdeskForm">
+                    <div class="mb-3">
+                        <label for="helpdeskName" class="form-label">Jméno</label>
+                        <input type="text" class="form-control" id="helpdeskName" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="helpdeskEmail" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="helpdeskEmail" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="helpdeskProblem" class="form-label">Typ problému</label>
+                        <select class="form-select" id="helpdeskProblem" required>
+                            <option value="">Vyberte problém</option>
+                            <option value="login">Zapomenuté přihlašovací údaje</option>
+                            <option value="download">Nelze stáhnout článek</option>
+                            <option value="other">Jiný problém</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="helpdeskDescription" class="form-label">Popis problému</label>
+                        <textarea class="form-control" id="helpdeskDescription" rows="3" required></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Odeslat</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Skript pro helpdesk -->
+<script>
+    document.getElementById('helpdeskForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        var name = document.getElementById('helpdeskName').value;
+        var email = document.getElementById('helpdeskEmail').value;
+        var problem = document.getElementById('helpdeskProblem').value;
+        var description = document.getElementById('helpdeskDescription').value;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'endpoints/submit_helpdesk.php', true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.onload = function() {
+            if (this.status == 200) {
+                // Zobrazte zprávu o úspěchu
+                alert('Váš požadavek byl úspěšně odeslán. Administrátor Vás bude kontaktovat na zadané e-mailové adrese.');
+                // Zavřete modální okno a vymažte formulář
+                $('#helpdeskModal').modal('hide');
+                document.getElementById('helpdeskForm').reset();
+            } else {
+                // Zpracování chyb
+                alert('Došlo k chybě při odesílání formuláře.');
+            }
+        };
+        xhr.send('name=' + encodeURIComponent(name) + '&email=' + encodeURIComponent(email) + '&problem=' + encodeURIComponent(problem) + '&description=' + encodeURIComponent(description));
+    });
+</script>
+
+
+
+
 
 <!-- Skript pro přihlášení -->
 <script>
